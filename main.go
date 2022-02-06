9d02959f69b357cf49c49aa7173e1ecd542c3ca5
Nikita Denisenok <nikitadenisenok@gmail.com>
2022-02-06 10:06:23 +0300

    add game

--- main.go
+++ main.go
@@ -1,10 +1,76 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+)
 
 func main() {
-	fmt.Println("Hello, World! Oh dear")
-	var a int = 0
-	fmt.Printf("%d /n", a)
+	Menu()
 }
-fff
+func ReadInput() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func Menu() {
+	fmt.Println("New game\nExit")
+
+outerLoop:
+	for {
+		command := ReadInput()
+		switch command {
+		case "New game":
+			ClearTerminal()
+			fmt.Println("Let the game begin")
+			StartGame()
+			break outerLoop
+		case "Exit":
+			break outerLoop
+		default:
+			fmt.Println("Enter a proper command", command, "blabla")
+			continue
+		}
+	}
+}
+func StartGame() {
+	var user1Inp, user2Inp int
+
+	fmt.Println("1P, please, enter the number")
+	fmt.Scan(&user1Inp)
+	ClearTerminal()
+	fmt.Println("2P, please, enter the number")
+
+guessLoop:
+	for fmt.Scan(&user2Inp); user1Inp != user2Inp; fmt.Scan(&user2Inp) {
+		if user2Inp > user1Inp {
+			fmt.Println("Greater")
+			continue guessLoop
+		} else if user2Inp < user1Inp {
+			fmt.Println("Smaller")
+			continue guessLoop
+		}
+	}
+
+	ClearTerminal()
+	fmt.Println("Bingo!")
+	Menu()
+}
+func ClearTerminal() {
+	switch runtime.GOOS {
+	case "linux":
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	case "windows":
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		fmt.Print("Ambiguous OS")
+	}
+}
